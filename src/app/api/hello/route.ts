9798e33854ab41bdfa938269b56e8d9794d63fce
gneiru <noelrohi59@gmail.com>
2023-06-03 15:59:50 +0800

    add query post in hello route

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,3 +3,9 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({ hello: 'world' });
 }
+
+export async function POST(req: Request) {
+    const { searchParams } = new URL(req.url);
+    const name = searchParams.get("name") ?? 'world';
+    return NextResponse.json({ hello: name });
+}
