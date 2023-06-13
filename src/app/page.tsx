b836a7eabfc34b0b02740018a823b672c635d664
gneiru <noelrohi59@gmail.com>
2023-06-13 18:53:10 +0800

    add `check the docs` link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       Hi there from Multus AQW Guild!
+      <Link href={"https://docs-multus.vercel.app/"} className="underline">Check the docs</Link>
     </main>
-  )
+  );
 }
